4212d6723005d1d1de802edd2b892cf0d4a0394a
Pim van der Loos <pimvanderloos@gmail.com>
2018-08-05 17:16:25 +0200

    Data values BugFix. - Fixed blocks such as andesite (1:5) being reset their base variant (1:0 in this case).

--- src/main/java/nl/pim16aap2/bigDoors/util/BlockData.java
+++ src/main/java/nl/pim16aap2/bigDoors/util/BlockData.java
@@ -1,6 +1,7 @@
 package nl.pim16aap2.bigDoors.util;
 
 import org.bukkit.Material;
+import org.bukkit.material.MaterialData;
 
 import nl.pim16aap2.bigDoors.customEntities.CustomCraftFallingBlock_Vall;
 
@@ -9,19 +10,22 @@ public class BlockData
 	private Material    				       mat;
 	private double      				    radius;
 	private CustomCraftFallingBlock_Vall	fBlock;
+	private MaterialData         	   matData;
 	private Byte     				 blockByte;
 	
-	public BlockData(Material mat, Byte blockByte, CustomCraftFallingBlock_Vall fBlock, double radius)
+	public BlockData(Material mat, Byte blockByte, CustomCraftFallingBlock_Vall fBlock, double radius, MaterialData matData)
 	{
 		this.mat       = mat;
 		this.fBlock    = fBlock;
 		this.radius    = radius;
+		this.matData   = matData;
 		this.blockByte = blockByte;
 	}
 
 	public void setFBlock(CustomCraftFallingBlock_Vall block)	{	this.fBlock = block;		}
 	public CustomCraftFallingBlock_Vall getFBlock()    		{	return this.fBlock;		}
 	public Material getMat()     							{	return this.mat;			}
+	public MaterialData getMatData()							{	return this.matData;		}
 	public double getRadius()								{	return this.radius;		}
 	public Byte getBlockByte()								{	return this.blockByte;	}
 }
