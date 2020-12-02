679f413d210699119af5a47afe2242ca372eedd9
Pim van der Loos <pimvanderloos@gmail.com>
2020-12-02 14:09:42 +0100

    Ignore cancellation status of chunk unloads
    - Whenever a chunk with active doors is unloaded, the animations will now be cancelled regardless of whether or not the unload is cancelled. This also goes for forceloaded chunks. Ignoring forceloaded chunks solves an issue with the server sometimes unloading forceloaded chunks regardless (looking at you, Tuinity).

--- src/main/java/nl/pim16aap2/bigDoors/handlers/ChunkUnloadHandler.java
+++ src/main/java/nl/pim16aap2/bigDoors/handlers/ChunkUnloadHandler.java
@@ -1,100 +1,27 @@
 package nl.pim16aap2.bigDoors.handlers;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
 import nl.pim16aap2.bigDoors.BigDoors;
-import nl.pim16aap2.bigDoors.BigDoors.MCVersion;
-import nl.pim16aap2.bigDoors.util.Util;
 
 public class ChunkUnloadHandler implements Listener
 {
     private final BigDoors plugin;
-    private final boolean useTheForce;
-    private boolean success = false;
 
     public ChunkUnloadHandler(BigDoors plugin)
     {
         this.plugin = plugin;
-        useTheForce = BigDoors.getMCVersion().equals(MCVersion.v1_14) ||
-                      BigDoors.getMCVersion().equals(MCVersion.v1_15) ||
-                      BigDoors.getMCVersion().equals(MCVersion.v1_16);
-        init();
-    }
-
-    // <1.14 method.
-    private Method isCancelled;
-
-    // 1.14 method.
-    private Method isForceLoaded;
-
-    private void init()
-    {
-        try
-        {
-            if (useTheForce)
-            {
-                isForceLoaded = org.bukkit.Chunk.class.getMethod("isForceLoaded");
-                success = true;
-            }
-            else
-            {
-                isCancelled = org.bukkit.event.world.ChunkUnloadEvent.class.getMethod("isCancelled");
-                success = true;
-            }
-        }
-        catch (NoSuchMethodException | SecurityException e)
-        {
-            success = false;
-            plugin.getMyLogger().logMessage("Serious error encountered! Unloading chunks with active doors IS UNSAFE!",
-                                            true, true);
-            plugin.getMyLogger().logMessageToLogFile(Util.exceptionToString(e));
-            e.printStackTrace();
-        }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onChunkUnload(ChunkUnloadEvent event)
     {
-        // If this class couldn't figure out reflection properly, give up.
-        if (!success)
-        {
-            plugin.getMyLogger()
-                .warn("ChunkUnloadHandler was not able to initialize properly! Please contact pim16aap2.");
-            return;
-        }
-
-        // If another plugin has already cancelled this event (or, forceLoaded this
-        // chunk in 1.14), there's no need to interfere.
-        if (isChunkUnloadCancelled(event))
-            return;
-
         // Abort all currently active BlockMovers that (might) interact with the chunk
         // that is being unloaded.
         plugin.getCommander().getBlockMovers().filter(BM -> BM.getDoor().chunkInRange(event.getChunk()))
             .forEach(BM -> BM.getDoor().setCanGo(false));
-    }
-
-    private boolean isChunkUnloadCancelled(ChunkUnloadEvent event)
-    {
-        try
-        {
-            if (useTheForce)
-                return (boolean) isForceLoaded.invoke(event.getChunk());
-            return (boolean) isCancelled.invoke(event);
-        }
-        catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
-        {
-            plugin.getMyLogger().logMessage("Serious error encountered! Unloading chunks with active doors IS UNSAFE!",
-                                            true, true);
-            plugin.getMyLogger().logMessageToLogFile(Util.exceptionToString(e));
-            e.printStackTrace();
-            return false;
-        }
     }
 }
