001b041af917a56436d832da9d6db10531bd1bee
Pim van der Loos <pimvanderloos@gmail.com>
2021-04-13 12:30:13 +0200

    Set up toggle command basics

--- bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/commands/Close.java
+++ bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/commands/Close.java
@@ -3,18 +3,32 @@ package nl.pim16aap2.bigdoors.commands;
 import lombok.NonNull;
 import lombok.ToString;
 import nl.pim16aap2.bigdoors.api.ICommandSender;
+import nl.pim16aap2.bigdoors.events.dooraction.DoorActionType;
+import nl.pim16aap2.bigdoors.util.DoorRetriever;
 
 @ToString
 public class Close extends Toggle
 {
-    public Close(@NonNull ICommandSender commandSender)
+    public Close(@NonNull ICommandSender commandSender, double speedMultiplier,
+                 @NonNull DoorRetriever... doorRetrievers)
     {
-        super(commandSender);
+        super(commandSender, speedMultiplier, doorRetrievers);
+    }
+
+    public Close(@NonNull ICommandSender commandSender, @NonNull DoorRetriever[] doorRetrievers)
+    {
+        super(commandSender, doorRetrievers);
     }
 
     @Override
     public @NonNull CommandDefinition getCommand()
     {
         return CommandDefinition.CLOSE;
+    }
+
+    @Override
+    public @NonNull DoorActionType getDoorActionType()
+    {
+        return DoorActionType.CLOSE;
     }
 }
--- bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/commands/Toggle.java
+++ bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/commands/Toggle.java
@@ -3,6 +3,7 @@ package nl.pim16aap2.bigdoors.commands;
 import lombok.NonNull;
 import lombok.ToString;
 import nl.pim16aap2.bigdoors.api.ICommandSender;
+import nl.pim16aap2.bigdoors.events.dooraction.DoorActionType;
 import nl.pim16aap2.bigdoors.util.DoorRetriever;
 import nl.pim16aap2.bigdoors.util.pair.BooleanPair;
 
@@ -14,19 +15,25 @@ public class Toggle extends BaseCommand
     private final @NonNull DoorRetriever[] doorRetrievers;
     private final double speedMultiplier;
 
-    public Toggle(@NonNull ICommandSender commandSender)
-    {
-        super(commandSender);
-        doorRetrievers = null;
-        speedMultiplier = 1;
-    }
-
     public Toggle(@NonNull ICommandSender commandSender, double speedMultiplier,
                   @NonNull DoorRetriever... doorRetrievers)
     {
         super(commandSender);
         this.speedMultiplier = speedMultiplier;
         this.doorRetrievers = doorRetrievers;
+    }
+
+    public Toggle(@NonNull ICommandSender commandSender, @NonNull DoorRetriever... doorRetrievers)
+    {
+        this(commandSender, 0D, doorRetrievers);
+    }
+
+    /**
+     * Gets the {@link DoorActionType} for this command.
+     */
+    public @NonNull DoorActionType getDoorActionType()
+    {
+        return DoorActionType.TOGGLE;
     }
 
     @Override
@@ -36,7 +43,7 @@ public class Toggle extends BaseCommand
     }
 
     @Override
-    protected @NonNull CompletableFuture<Boolean> executeCommand(@NonNull BooleanPair permissions)
+    protected final @NonNull CompletableFuture<Boolean> executeCommand(@NonNull BooleanPair permissions)
     {
         throw new UnsupportedOperationException("This command has not yet been implemented!");
     }
