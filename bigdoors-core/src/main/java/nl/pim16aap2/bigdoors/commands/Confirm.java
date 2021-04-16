0b2a0febe53116fdf4fd4b92baa4a23845aeee56
Pim van der Loos <pimvanderloos@gmail.com>
2021-04-16 15:05:04 +0200

    Implement confirm command

--- bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/commands/Confirm.java
+++ bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/commands/Confirm.java
@@ -2,7 +2,10 @@ package nl.pim16aap2.bigdoors.commands;
 
 import lombok.NonNull;
 import lombok.ToString;
+import lombok.val;
+import nl.pim16aap2.bigdoors.BigDoors;
 import nl.pim16aap2.bigdoors.api.ICommandSender;
+import nl.pim16aap2.bigdoors.api.IPPlayer;
 import nl.pim16aap2.bigdoors.util.pair.BooleanPair;
 
 import java.util.concurrent.CompletableFuture;
@@ -24,6 +27,19 @@ public class Confirm extends BaseCommand
     @Override
     protected @NonNull CompletableFuture<Boolean> executeCommand(final @NonNull BooleanPair permissions)
     {
-        throw new UnsupportedOperationException("This command has not yet been implemented!");
+        if (!(getCommandSender() instanceof IPPlayer))
+        {
+            getCommandSender().sendMessage("Only players can use this command!");
+            return CompletableFuture.completedFuture(true);
+        }
+
+        val toolUser = BigDoors.get().getToolUserManager().getToolUser(((IPPlayer) getCommandSender()).getUUID());
+        if (toolUser.isPresent())
+            toolUser.get().handleInput(true);
+        else
+            // TODO: Localization.
+            getCommandSender().sendMessage("Nothing to confirm!");
+
+        return CompletableFuture.completedFuture(true);
     }
 }
