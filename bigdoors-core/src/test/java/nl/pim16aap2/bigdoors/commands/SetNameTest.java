d74d0afe868d973b0030159eae1548d477e69f4e
Pim van der Loos <pimvanderloos@gmail.com>
2021-04-23 11:53:20 +0200

    Add test for SetName

---
+++ bigdoors-core/src/test/java/nl/pim16aap2/bigdoors/commands/SetNameTest.java
@@ -0,0 +1,75 @@
+package nl.pim16aap2.bigdoors.commands;
+
+import lombok.SneakyThrows;
+import lombok.val;
+import nl.pim16aap2.bigdoors.api.IBigDoorsPlatform;
+import nl.pim16aap2.bigdoors.api.IPPlayer;
+import nl.pim16aap2.bigdoors.managers.ToolUserManager;
+import nl.pim16aap2.bigdoors.tooluser.creator.Creator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static nl.pim16aap2.bigdoors.commands.CommandTestingUtil.initCommandSenderPermissions;
+import static nl.pim16aap2.bigdoors.commands.CommandTestingUtil.initPlatform;
+
+class SetNameTest
+{
+    @Mock
+    private IPPlayer commandSender;
+
+    @Mock
+    private ToolUserManager toolUserManager;
+
+    private IBigDoorsPlatform platform;
+
+    @BeforeEach
+    void init()
+    {
+        platform = initPlatform();
+        MockitoAnnotations.openMocks(this);
+        initCommandSenderPermissions(commandSender, true, true);
+
+        Mockito.when(platform.getToolUserManager()).thenReturn(toolUserManager);
+    }
+
+    @Test
+    @SneakyThrows
+    void testInput()
+    {
+        Assertions.assertFalse(SetName.run(commandSender, "1").get(1, TimeUnit.SECONDS));
+        Assertions.assertTrue(SetName.run(commandSender, "newDoor").get(1, TimeUnit.SECONDS));
+    }
+
+    @Test
+    @SneakyThrows
+    void testExecution()
+    {
+        val uuid = UUID.randomUUID();
+        val name = "newDoor";
+
+        val toolUser = Mockito.mock(Creator.class);
+        Mockito.when(commandSender.getUUID()).thenReturn(uuid);
+        Mockito.when(toolUserManager.getToolUser(uuid)).thenReturn(Optional.of(toolUser));
+
+        Assertions.assertTrue(SetName.run(commandSender, name).get(1, TimeUnit.SECONDS));
+
+        Mockito.verify(toolUser).handleInput(name);
+    }
+
+    @Test
+    @SneakyThrows
+    void testServer()
+    {
+        Assertions.assertTrue(SetName.run(Mockito.mock(IPServer.class, Answers.CALLS_REAL_METHODS), "newDoor")
+                                     .get(1, TimeUnit.SECONDS));
+    }
+}
