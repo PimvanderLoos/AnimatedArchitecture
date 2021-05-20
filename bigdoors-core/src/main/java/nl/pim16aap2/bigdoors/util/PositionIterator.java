6d53cf66f9cd3eefe1f629c1a1bc3350338504b7
Pim van der Loos <pimvanderloos@gmail.com>
2021-05-20 19:25:13 +0200

    Clean up PositionIterator class
    - Cleaned up the PositionIterator class. While it should've worked fine before, it is a bit more logical now. Additionally, it is now marked as Deprecated to properly reflect the current status of the class.

--- bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/util/PositionIterator.java
+++ bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/util/PositionIterator.java
@@ -1,22 +1,54 @@
 package nl.pim16aap2.bigdoors.util;
 
 import lombok.NonNull;
+import nl.pim16aap2.bigdoors.util.functional.TriIntConsumer;
 import nl.pim16aap2.bigdoors.util.vector.Vector3Di;
+import nl.pim16aap2.bigdoors.util.vector.Vector3DiConst;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
  * Iterator that iterates over all values between 2 {@link Vector3Di}s.
  *
  * @author Pim
+ * @deprecated This class does not have any tests and is likely to be removed in the future!
  */
+@Deprecated
 public class PositionIterator implements Iterable<Vector3Di>
 {
-private final @NonNull Vector3Di posA;
-private final @NonNull Vector3Di posB;
-private final @NonNull IterationMode iterationMode;
+    private final @NonNull Vector3DiConst posA;
+    private final @NonNull Vector3DiConst posB;
+    private final @NonNull IterationMode iterationMode;
     private final int dx, dy, dz;
+    private final int volume;
+
+    /**
+     * Construct a new PositionIterator. When iterating between two values, the starting values are the first result and
+     * the ending values are the last result. If the starting and ending values are the same, there will be a single
+     * result.
+     *
+     * @param posA          The starting values.
+     * @param posB          The ending values.
+     * @param iterationMode The mode of iteration.
+     * @param volume        The total number of blocks between posA and posB (inclusive). Do not use this if you do not
+     *                      know this.
+     */
+    public PositionIterator(final @NonNull Vector3DiConst posA, final @NonNull Vector3DiConst posB,
+                            final @NonNull IterationMode iterationMode, final int volume)
+    {
+        if (volume < 1)
+            throw new IllegalArgumentException("Volume " + volume + " cannot be smaller than 1 block!");
+        this.posA = posA;
+        this.posB = posB;
+        this.iterationMode = iterationMode;
+        this.volume = volume;
+        dx = posA.getX() > posB.getX() ? -1 : 1;
+        dy = posA.getY() > posB.getY() ? -1 : 1;
+        dz = posA.getZ() > posB.getZ() ? -1 : 1;
+    }
 
     /**
      * Construct a new PositionIterator. When iterating between two values, the starting values are the first result and
@@ -27,21 +59,46 @@ private final @NonNull IterationMode iterationMode;
      * @param posB          The ending values.
      * @param iterationMode The mode of iteration.
      */
-    public PositionIterator(final @NonNull Vector3Di posA, final @NonNull Vector3Di posB,
+    public PositionIterator(final @NonNull Vector3DiConst posA, final @NonNull Vector3DiConst posB,
                             final @NonNull IterationMode iterationMode)
     {
-        this.posA = posA;
-        this.posB = posB;
-        this.iterationMode = iterationMode;
-        dx = posA.getX() > posB.getX() ? -1 : 1;
-        dy = posA.getY() > posB.getY() ? -1 : 1;
-        dz = posA.getZ() > posB.getZ() ? -1 : 1;
+        this(posA, posB, iterationMode, getVolume(posA, posB));
+    }
+
+    /**
+     * Construct a new PositionIterator. When iterating between two values, the starting values are the first result and
+     * the ending values are the last result. If the starting and ending values are the same, there will be a single
+     * result.
+     *
+     * @param cuboid        The {@link Cuboid} to iterate over, from {@link Cuboid#getMin()} to {@link
+     *                      Cuboid#getMax()}.
+     * @param iterationMode The mode of iteration.
+     */
+    public PositionIterator(final @NonNull Cuboid cuboid, final @NonNull IterationMode iterationMode)
+    {
+        this(cuboid.getMin(), cuboid.getMax(), iterationMode, cuboid.getVolume());
+    }
+
+    private static int getVolume(final @NonNull Vector3DiConst posA, final @NonNull Vector3DiConst posB)
+    {
+        return new CuboidConst(posA, posB).getVolume();
     }
 
     @Override
-    public @NonNull Iterator<Vector3Di> iterator()
+    public @NonNull CustomIterator iterator()
     {
         return new CustomIterator(this);
+    }
+
+    /**
+     * Works similar to {@link #forEach(Consumer)}, but with the exception that each step will be applied without
+     * constructing a new {@link Vector3Di}.
+     *
+     * @param action The method to apply for each step.
+     */
+    public void forEach(final @NonNull TriIntConsumer action)
+    {
+        iterator().forEach(action);
     }
 
     /**
@@ -51,7 +108,8 @@ private final @NonNull IterationMode iterationMode;
     {
         private int x, y, z;
         private final Supplier<Boolean> outerLoop, middleLoop, innerLoop;
-        private boolean firstValue = true;
+        private final Runnable resetMiddleLoop, resetInnerLoop;
+        private int currentIndex = 0;
 
         private CustomIterator(final @NonNull PositionIterator locationIterator)
         {
@@ -61,6 +119,15 @@ private final @NonNull IterationMode iterationMode;
             outerLoop = getIncrementor(locationIterator.iterationMode.getIndex(0));
             middleLoop = getIncrementor(locationIterator.iterationMode.getIndex(1));
             innerLoop = getIncrementor(locationIterator.iterationMode.getIndex(2));
+
+            resetMiddleLoop = getResetMethod(locationIterator.iterationMode.getIndex(1));
+            resetInnerLoop = getResetMethod(locationIterator.iterationMode.getIndex(2));
+        }
+
+        private void forEach(final @NonNull TriIntConsumer action)
+        {
+            do action.accept(x, y, z);
+            while (step());
         }
 
         /**
@@ -71,67 +138,103 @@ private final @NonNull IterationMode iterationMode;
          */
         private Supplier<Boolean> getIncrementor(final Axis axis)
         {
-            switch (axis)
-            {
-                case X:
-                    return this::incrementX;
-                case Y:
-                    return this::incrementY;
-                case Z:
-                    return this::incrementZ;
-            }
-            return null;
+            return switch (axis)
+                {
+                    case X -> this::incrementX;
+                    case Y -> this::incrementY;
+                    case Z -> this::incrementZ;
+                };
         }
 
         /**
-         * Increments {@link #x} by a step of {@link #dx}. If the value of X matches {@link #posB}.x, then it's reset to
-         * {@link #posA}.x.
+         * Increments {@link #x} by a step of {@link #dx} if possible.
+         * <p>
+         * If no new values are available, i.e. {@link #x} == {@link Vector3Di#getX()} for {@link #posB}, this method
+         * does nothing other than returning false.
          *
-         * @return True if the value of X was reset to {@link #posA}.x.
+         * @return True if the x axis could be updated, otherwise false.
          */
         private boolean incrementX()
         {
             if (x == posB.getX())
-            {
-                x = posA.getX();
-                return true;
-            }
+                return false;
+
             x += dx;
-            return false;
+            return true;
         }
 
         /**
-         * Increments {@link #y} by a step of {@link #dy}. If the value of Y matches {@link #posB}.y, then it's reset to
-         * {@link #posA}.y.
+         * Increments {@link #y} by a step of {@link #dy} if possible.
+         * <p>
+         * If no new values are available, i.e. {@link #y} == {@link Vector3Di#getY()} for {@link #posB}, this method
+         * does nothing other than returning false.
          *
-         * @return True if the value of Y was reset to {@link #posA}.y.
+         * @return True if the y axis could be updated, otherwise false.
          */
         private boolean incrementY()
         {
             if (y == posB.getY())
-            {
-                y = posA.getY();
-                return true;
-            }
+                return false;
+
             y += dy;
-            return false;
+            return true;
         }
 
         /**
-         * Increments {@link #z} by a step of {@link #dz}. If the value of Z matches {@link #posB}.z, then it's reset to
-         * {@link #posA}.z.
+         * Increments {@link #z} by a step of {@link #dz} if possible.
+         * <p>
+         * If no new values are available, i.e. {@link #z} == {@link Vector3Di#getZ()} for {@link #posB}, this method
+         * does nothing other than returning false.
          *
-         * @return True if the value of Z was reset to {@link #posA}.z.
+         * @return True if the z axis could be updated, otherwise false.
          */
         private boolean incrementZ()
         {
             if (z == posB.getZ())
-            {
-                z = posA.getZ();
-                return true;
-            }
+                return false;
+
             z += dz;
-            return false;
+            return true;
+        }
+
+        /**
+         * Gets the reset runnable for a specified {@link #PositionIterator ::Axis}.
+         *
+         * @param axis The {@link #PositionIterator ::Axis}.
+         * @return The runnable that increments an {@link #PositionIterator ::Axis}.
+         */
+        private Runnable getResetMethod(final Axis axis)
+        {
+            return switch (axis)
+                {
+                    case X -> this::resetX;
+                    case Y -> this::resetY;
+                    case Z -> this::resetZ;
+                };
+        }
+
+        /**
+         * Resets the X axis to the starting value.
+         */
+        private void resetX()
+        {
+            x = posA.getX();
+        }
+
+        /**
+         * Resets the Y axis to the starting value.
+         */
+        private void resetY()
+        {
+            y = posA.getY();
+        }
+
+        /**
+         * Resets the Z axis to the starting value.
+         */
+        private void resetZ()
+        {
+            z = posA.getZ();
         }
 
         /**
@@ -140,7 +243,7 @@ private final @NonNull IterationMode iterationMode;
         @Override
         public boolean hasNext()
         {
-            return firstValue || !(x == posB.getX() && y == posB.getY() && z == posB.getZ());
+            return currentIndex < volume;
         }
 
         /**
@@ -149,16 +252,24 @@ private final @NonNull IterationMode iterationMode;
         @Override
         public Vector3Di next()
         {
-            if (firstValue)
-            {
-                firstValue = false;
-                return new Vector3Di(posA.getX(), posA.getY(), posA.getZ());
-            }
-
-            if (innerLoop.get())
-                if (middleLoop.get())
-                    outerLoop.get();
+            if (!step())
+                throw new NoSuchElementException();
             return new Vector3Di(x, y, z);
+        }
+
+        private boolean step()
+        {
+            ++currentIndex;
+            if (!innerLoop.get())
+            {
+                resetInnerLoop.run();
+                if (!middleLoop.get())
+                {
+                    resetMiddleLoop.run();
+                    return outerLoop.get();
+                }
+            }
+            return true;
         }
     }
 
