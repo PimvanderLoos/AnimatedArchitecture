f678b8e9c1d9c23e4df8e7e83d9a71be8a1b70a0
Pim van der Loos <pimvanderloos@gmail.com>
2021-08-20 17:43:40 +0200

    Revert to CheckerFramework default nullability
    - Reverted back to the CheckerFramework's nullability annotation. The reason for switching to Eclipse's in the first place was that it didn't cause some of the issues CF's did. However, it turned out that Eclipse's one needed to be explicitly applied to every package. After doing that, the same issues popped back up, so it was just more work without any gain.

--- bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/package-info.java
+++ bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/package-info.java
@@ -1,4 +1,5 @@
-@NonNullByDefault
+@DefaultQualifier(NotNull.class)
 package nl.pim16aap2.bigdoors;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.jetbrains.annotations.NotNull;
