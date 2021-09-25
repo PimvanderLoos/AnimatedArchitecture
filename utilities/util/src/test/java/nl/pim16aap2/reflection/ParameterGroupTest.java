cac67a7a7e60bf46a51f86ebb0d9d35198bb40b9
Pim van der Loos <pimvanderloos@gmail.com>
2021-09-25 12:13:37 +0200

    Add test for ParameterGroup class

---
+++ utilities/util/src/test/java/nl/pim16aap2/reflection/ParameterGroupTest.java
@@ -0,0 +1,39 @@
+package nl.pim16aap2.reflection;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ParameterGroupTest
+{
+    @Test
+    void testBasic()
+    {
+        Class<?>[] base = new Class[]{int.class, Object.class, boolean.class};
+        ParameterGroup pg = new ParameterGroup.Builder().withRequiredParameters(base).construct();
+        Assertions.assertTrue(pg.matches(base));
+
+        pg = new ParameterGroup.Builder().withRequiredParameters(base).withRequiredParameters(Object.class).construct();
+        Assertions.assertFalse(pg.matches(base));
+
+        pg = new ParameterGroup.Builder().withRequiredParameters(Object.class, int.class, Object.class).construct();
+        Assertions.assertFalse(pg.matches(base));
+    }
+
+    @Test
+    void testOptional()
+    {
+        Class<?>[] base = new Class[]{int.class, Object.class, boolean.class};
+
+        ParameterGroup pg = new ParameterGroup.Builder()
+            .withOptionalParameters(boolean.class)
+            .withRequiredParameters(int.class)     // <--- 0
+            .withRequiredParameters(Object.class)  // <--- 1
+            .withOptionalParameters(java.util.List.class)
+            .withRequiredParameters(boolean.class) // <--- 2
+            .withOptionalParameters(java.util.List.class).construct();
+        Assertions.assertTrue(pg.matches(base));
+
+        pg = new ParameterGroup.Builder(pg).withRequiredParameters(Object.class).construct();
+        Assertions.assertFalse(pg.matches(base));
+    }
+}
