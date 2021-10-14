f02b6740dc9667032c133dc4f6b11724877e9111
Pim van der Loos <pimvanderloos@gmail.com>
2021-10-14 23:14:11 +0200

    Update Lands compat hook

--- core/src/main/java/nl/pim16aap2/bigDoors/compatiblity/LandsProtectionCompat.java
+++ core/src/main/java/nl/pim16aap2/bigDoors/compatiblity/LandsProtectionCompat.java
@@ -2,14 +2,15 @@ package nl.pim16aap2.bigDoors.compatiblity;
 
 import java.util.UUID;
 
+import me.angeschossen.lands.api.flags.Flags;
+import me.angeschossen.lands.api.land.Area;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import me.angeschossen.lands.api.integration.LandsIntegration;
-import me.angeschossen.lands.api.land.LandChunk;
-import me.angeschossen.lands.api.role.enums.RoleSetting;
 import nl.pim16aap2.bigDoors.BigDoors;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Compatibility hook for Lands:
@@ -20,16 +21,12 @@ import nl.pim16aap2.bigDoors.BigDoors;
  */
 public class LandsProtectionCompat implements IProtectionCompat
 {
-    private final BigDoors plugin;
-    private boolean success = false;
     private final LandsIntegration landsAddon;
     private static final ProtectionCompat compat = ProtectionCompat.LANDS;
 
     public LandsProtectionCompat(BigDoors plugin)
     {
-        this.plugin = plugin;
-        landsAddon = new LandsIntegration(plugin, false);
-        success = landsAddon != null;
+        landsAddon = new LandsIntegration(plugin);
     }
 
     /**
@@ -38,9 +35,10 @@ public class LandsProtectionCompat implements IProtectionCompat
     @Override
     public boolean canBreakBlock(Player player, Location loc)
     {
-        return landsAddon
-                .getLandChunk(loc)
-                .canAction(player.getUniqueId(), RoleSetting.BLOCK_BREAK);
+        final @Nullable Area area = landsAddon.getAreaByLoc(loc);
+        if (area == null)
+            return true;
+        return area.hasFlag(player.getUniqueId(), Flags.BLOCK_BREAK);
     }
 
     /**
@@ -49,21 +47,26 @@ public class LandsProtectionCompat implements IProtectionCompat
     @Override
     public boolean canBreakBlocksBetweenLocs(Player player, Location loc1, Location loc2)
     {
-        UUID playerUUID = player.getUniqueId();
-        World world = loc1.getWorld();
+        final UUID playerUUID = player.getUniqueId();
+        final World world = loc1.getWorld();
 
         int x1 = Math.min(loc1.getBlockX(), loc2.getBlockX()) >> 4;
         int z1 = Math.min(loc1.getBlockZ(), loc2.getBlockZ()) >> 4;
         int x2 = Math.max(loc1.getBlockX(), loc2.getBlockX()) >> 4;
         int z2 = Math.max(loc1.getBlockZ(), loc2.getBlockZ()) >> 4;
 
+        final Location loc = new Location(world, 0, 128, 0);
+
         for (int chunkX = x1; chunkX <= x2; ++chunkX)
             for (int chunkZ = z1; chunkZ <= z2; ++chunkZ)
             {
-                final LandChunk landChunk = landsAddon.getLandChunk(new Location(world, chunkX << 4, 64, chunkZ << 4));
-                if (landChunk == null)
+                loc.setX(x1 >> 4);
+                loc.setZ(z1 >> 4);
+
+                final @Nullable Area area = landsAddon.getAreaByLoc(loc);
+                if (area == null)
                     continue;
-                if (!landChunk.canAction(playerUUID, RoleSetting.BLOCK_BREAK))
+                if(!area.hasFlag(playerUUID, Flags.BLOCK_BREAK))
                     return false;
             }
         return true;
@@ -75,7 +78,7 @@ public class LandsProtectionCompat implements IProtectionCompat
     @Override
     public boolean success()
     {
-        return success;
+        return true;
     }
 
     /**
