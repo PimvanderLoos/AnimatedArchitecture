0583f6ca4276c74577d5b3dcf17474d4750db04e
Pim van der Loos <pimvanderloos@gmail.com>
2022-03-20 16:45:28 +0100

    Add interface for Spigot animated block hooks
    - Added a simple interface for Spigot for IAnimatedBlockHook. It just makes it a bit nicer to write a hook for Spigot as you don't have to deal with generics.

---
+++ bigdoors-spigot/spigot-util/src/main/java/nl/pim16aap2/bigdoors/spigot/util/api/IAnimatedBlockHookSpigot.java
@@ -0,0 +1,7 @@
+package nl.pim16aap2.bigdoors.spigot.util.api;
+
+import nl.pim16aap2.bigdoors.api.IAnimatedBlockHook;
+
+public interface IAnimatedBlockHookSpigot extends IAnimatedBlockHook<IAnimatedBlockSpigot>
+{
+}
