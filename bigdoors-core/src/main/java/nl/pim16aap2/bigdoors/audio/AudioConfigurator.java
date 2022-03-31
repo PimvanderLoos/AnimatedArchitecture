f910eb444c5445a2a4d9ef80002040aec869420f
Pim van der Loos <pimvanderloos@gmail.com>
2022-03-31 15:02:02 +0200

    Use door type audio in audio configurator

--- bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/audio/AudioConfigurator.java
+++ bigdoors-core/src/main/java/nl/pim16aap2/bigdoors/audio/AudioConfigurator.java
@@ -6,6 +6,7 @@ import nl.pim16aap2.bigdoors.api.restartable.IRestartable;
 import nl.pim16aap2.bigdoors.api.restartable.RestartableHolder;
 import nl.pim16aap2.bigdoors.doors.AbstractDoor;
 import nl.pim16aap2.bigdoors.doortypes.DoorType;
+import nl.pim16aap2.bigdoors.managers.DoorTypeManager;
 import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
@@ -14,31 +15,57 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a class used to read and access the audio configuration.
+ *
+ * @author Pim
+ */
 public final class AudioConfigurator implements IRestartable, IDebuggable
 {
+    private static final AudioSet EMPTY_AUDIO_SET = new AudioSet(null, null);
+
     private final Path file;
-    private @Nullable AudioSet defaultAudio = null;
+    private final DoorTypeManager doorTypeManager;
+    private AudioSet defaultAudioSet = EMPTY_AUDIO_SET;
     private final Map<DoorType, AudioSet> audioMap = new HashMap<>();
 
     @Inject//
     AudioConfigurator(
         @Named("pluginBaseDirectory") Path baseDir, RestartableHolder restartableHolder,
-        DebuggableRegistry debuggableRegistry)
+        DebuggableRegistry debuggableRegistry, DoorTypeManager doorTypeManager)
     {
         file = baseDir.resolve("audio_config.json");
+        this.doorTypeManager = doorTypeManager;
 
         restartableHolder.registerRestartable(this);
         debuggableRegistry.registerDebuggable(this);
     }
 
+    /**
+     * Retrieves the audio set mapped for a door.
+     * <p>
+     * This method will use the user-specified configuration for retrieving the mapping. If the user did not modify
+     * this, the mapping is simply the same as the door type's default audio set.
+     *
+     * @param door
+     *     The door for which to retrieve the audio set to use.
+     * @return The audio set mapped for the given door.
+     */
     public AudioSet getAudioSet(AbstractDoor door)
     {
-        return new AudioSet(null, null);
+        final @Nullable AudioSet typeAudioSet = door.getDoorType().getAudioSet();
+        final AudioSet fallback = typeAudioSet == null ? defaultAudioSet : typeAudioSet;
+
+        final @Nullable AudioSet result = audioMap.putIfAbsent(door.getDoorType(), fallback);
+        return result == null ? fallback : result;
     }
 
     private void processAudioConfig()
     {
+        doorTypeManager.getEnabledDoorTypes().forEach(
+            type -> audioMap.put(type, type.getAudioSet() == null ? defaultAudioSet : type.getAudioSet()));
 
+        // TODO: Implement config
     }
 
     @Override
@@ -51,7 +78,7 @@ public final class AudioConfigurator implements IRestartable, IDebuggable
     public synchronized void shutDown()
     {
         audioMap.clear();
-        defaultAudio = null;
+        defaultAudioSet = EMPTY_AUDIO_SET;
     }
 
     @Override
@@ -59,7 +86,7 @@ public final class AudioConfigurator implements IRestartable, IDebuggable
     {
         final StringBuilder sb = new StringBuilder();
         sb.append("AudioConfigReader File: '").append(file).append("'\n")
-          .append("Default: ").append(defaultAudio).append('\n')
+          .append("Default: ").append(defaultAudioSet).append('\n')
           .append("AudioSets:\n");
         audioMap.forEach((key, val) -> sb.append("  Type: ").append(key).append(", Audio: ").append(val).append('\n'));
         return sb.toString();
