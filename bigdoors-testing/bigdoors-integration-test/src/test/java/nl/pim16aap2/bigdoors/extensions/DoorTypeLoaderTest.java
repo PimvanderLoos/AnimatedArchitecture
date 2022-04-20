af084ec0910ef62469a34999b6785065172b775a
Pim van der Loos <pimvanderloos@gmail.com>
2022-04-20 14:13:27 +0200

    Resolve dependencies for integration test
    - Because the DoorTypes now use relocated (shaded) dependencies instead of the actual dependencies (which does not work when loading them at runtime using shaded dependencies), the integration tests failed, as these used the actual dependencies. This meant that the door classes would use nl.pim16aap2.bigdoors.lib.flogger.FluentLogger, while it was only available at com.google.common.flogger.FluentLogger. To resolve this issue, a new module was introduced that is only meant to package all dependencies in a single jar. This is the bigdoors-integration-test-lib module. The bigdoors-integration-test then depends on that library module, which then means that all dependencies are available in their relocated positions.

--- bigdoors-testing/bigdoors-integration-test/src/test/java/nl/pim16aap2/bigdoors/extensions/DoorTypeLoaderTest.java
+++ bigdoors-testing/bigdoors-integration-test/src/test/java/nl/pim16aap2/bigdoors/extensions/DoorTypeLoaderTest.java
@@ -2,6 +2,7 @@ package nl.pim16aap2.bigdoors.extensions;
 
 import nl.pim16aap2.bigdoors.api.restartable.RestartableHolder;
 import nl.pim16aap2.bigdoors.managers.DoorTypeManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,22 +22,32 @@ class DoorTypeLoaderTest
     @Mock
     private DoorTypeManager doorTypeManager;
 
+    private AutoCloseable mocks;
+
     @BeforeEach
     public void init()
     {
-        MockitoAnnotations.openMocks(this);
+        mocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void cleanup()
+        throws Exception
+    {
+        mocks.close();
     }
 
     @Test
     void test()
         throws IOException
     {
+        final Path basePath = Path.of("");
         final String extensionsPath =
-            new File(".").getCanonicalPath()
-                         .replace("bigdoors-integration-test", "bigdoors-doors" + File.separator + "DoorTypes");
+            basePath.toAbsolutePath().getParent().getParent().resolve("bigdoors-doors")
+                    .resolve("DoorTypes").toAbsolutePath().toString();
         final int inputCount = Objects.requireNonNull(new File(extensionsPath).list()).length;
 
-        final var doorTypeLoader = new DoorTypeLoader(restartableHolder, doorTypeManager, Path.of(""));
+        final var doorTypeLoader = new DoorTypeLoader(restartableHolder, doorTypeManager, basePath);
         doorTypeLoader.initialize();
         Assertions.assertEquals(inputCount, doorTypeLoader.loadDoorTypesFromDirectory(Path.of(extensionsPath)).size());
     }
