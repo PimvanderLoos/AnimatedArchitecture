44ce320e4c5e5cf6443de2cdcce891097626ffe2
Pim van der Loos <pimvanderloos@gmail.com>
2023-02-14 15:47:23 +0100

    Delete Elevator type
    - The elevator type does not offer anything not already part of the portcullis type. While it would be fun to have proper elevators, I will leave that to external plugins or future-me to implement.

--- structures/structure-elevator/src/main/java/nl/pim16aap2/bigdoors/structures/elevator/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains all the data for the elevator structure type. The package includes the creator for the
- * structure, the definition of the type, the class for the type itself, and the animation component for the type.
- */
-@NonNullByDefault
-package nl.pim16aap2.bigdoors.structures.elevator;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
