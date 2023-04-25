50ddb5624f96c02d8ae1baf186a08c4d55a5733e
Pim van der Loos <pimvanderloos@gmail.com>
2023-04-25 14:55:26 +0200

    Add audio attenuation to audio player

--- animatedarchitecture-core/src/main/java/nl/pim16aap2/animatedarchitecture/core/audio/IAudioPlayer.java
+++ animatedarchitecture-core/src/main/java/nl/pim16aap2/animatedarchitecture/core/audio/IAudioPlayer.java
@@ -2,16 +2,22 @@ package nl.pim16aap2.animatedarchitecture.core.audio;
 
 import nl.pim16aap2.animatedarchitecture.core.api.ILocation;
 import nl.pim16aap2.animatedarchitecture.core.api.IWorld;
-import nl.pim16aap2.animatedarchitecture.core.util.vector.Vector3Dd;
-import nl.pim16aap2.animatedarchitecture.core.util.vector.Vector3Di;
+import nl.pim16aap2.animatedarchitecture.core.util.vector.IVector3D;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.DoubleUnaryOperator;
 
 /**
  * Represents an object that can play sounds.
- *
- * @author Pim
  */
+@SuppressWarnings("unused")
 public interface IAudioPlayer
 {
+    /**
+     * The radius in blocks around the sound source where players can hear the sound.
+     */
+    double AUDIO_RANGE = 15;
+
     /**
      * Play a sound for all players in a range of 15 blocks around the provided location.
      *
@@ -24,7 +30,10 @@ public interface IAudioPlayer
      * @param pitch
      *     The pitch
      */
-    void playSound(ILocation loc, String sound, float volume, float pitch);
+    default void playSound(ILocation loc, String sound, float volume, float pitch)
+    {
+        playSound(loc.getX(), loc.getY(), loc.getZ(), loc.getWorld(), sound, volume, pitch, AUDIO_RANGE, null);
+    }
 
     /**
      * Play a sound for all players in a range of 15 blocks around the provided location.
@@ -40,23 +49,10 @@ public interface IAudioPlayer
      * @param pitch
      *     The pitch
      */
-    void playSound(Vector3Di pos, IWorld world, String sound, float volume, float pitch);
-
-    /**
-     * Play a sound for all players in a range of 15 blocks around the provided location.
-     *
-     * @param pos
-     *     The position of the sound.
-     * @param world
-     *     The world of the position.
-     * @param sound
-     *     The sound to play.
-     * @param volume
-     *     The volume
-     * @param pitch
-     *     The pitch
-     */
-    void playSound(Vector3Dd pos, IWorld world, String sound, float volume, float pitch);
+    default void playSound(IVector3D pos, IWorld world, String sound, float volume, float pitch)
+    {
+        playSound(pos.xD(), pos.yD(), pos.zD(), world, sound, volume, pitch, AUDIO_RANGE, null);
+    }
 
     /**
      * Play a sound for all players in a range of 15 blocks around the provided location.
@@ -76,5 +72,64 @@ public interface IAudioPlayer
      * @param pitch
      *     The pitch
      */
-    void playSound(double x, double y, double z, IWorld world, String sound, float volume, float pitch);
+    default void playSound(double x, double y, double z, IWorld world, String sound, float volume, float pitch)
+    {
+        playSound(x, y, z, world, sound, volume, pitch, AUDIO_RANGE);
+    }
+
+    /**
+     * Play a sound for all players in a range of 15 blocks around the provided location.
+     *
+     * @param x
+     *     The x coordinate of the location.
+     * @param y
+     *     The y coordinate of the location.
+     * @param z
+     *     The z coordinate of the location.
+     * @param world
+     *     The world of the position.
+     * @param sound
+     *     The sound to play.
+     * @param volume
+     *     The volume
+     * @param pitch
+     *     The pitch
+     * @param range
+     *     The range of the sound.
+     */
+    default void playSound(
+        double x, double y, double z, IWorld world, String sound, float volume, float pitch, double range)
+    {
+        playSound(x, y, z, world, sound, volume, pitch, range, null);
+    }
+
+    /**
+     * Play a sound for all players in a range of 15 blocks around the provided location.
+     *
+     * @param x
+     *     The x coordinate of the location.
+     * @param y
+     *     The y coordinate of the location.
+     * @param z
+     *     The z coordinate of the location.
+     * @param world
+     *     The world of the position.
+     * @param sound
+     *     The sound to play.
+     * @param volume
+     *     The volume
+     * @param pitch
+     *     The pitch
+     * @param range
+     *     The range of the sound.
+     * @param attenuationFunction
+     *     The function that determines how the volume of the sound changes with distance. If null, the volume will not
+     *     change. The function takes the distance as input and returns the multiplier for the volume.
+     *     <p>
+     *     For example, if the function returns 0.5 for a distance of 10, the volume will be multiplied by 0.5 for all
+     *     players that are 10 blocks away from the sound.
+     */
+    void playSound(
+        double x, double y, double z, IWorld world, String sound, float volume, float pitch, double range,
+        @Nullable DoubleUnaryOperator attenuationFunction);
 }
--- animatedarchitecture-spigot/spigot-core/src/main/java/nl/pim16aap2/animatedarchitecture/spigot/core/implementations/AudioPlayerSpigot.java
+++ animatedarchitecture-spigot/spigot-core/src/main/java/nl/pim16aap2/animatedarchitecture/spigot/core/implementations/AudioPlayerSpigot.java
@@ -1,15 +1,10 @@
 package nl.pim16aap2.animatedarchitecture.spigot.core.implementations;
 
 import nl.pim16aap2.animatedarchitecture.core.api.IExecutor;
-import nl.pim16aap2.animatedarchitecture.core.api.ILocation;
 import nl.pim16aap2.animatedarchitecture.core.api.IWorld;
 import nl.pim16aap2.animatedarchitecture.core.audio.IAudioPlayer;
-import nl.pim16aap2.animatedarchitecture.core.util.vector.Vector3Dd;
-import nl.pim16aap2.animatedarchitecture.core.util.vector.Vector3Di;
-import nl.pim16aap2.animatedarchitecture.spigot.util.SpigotAdapter;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.SoundCategory;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -17,6 +12,7 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.function.DoubleUnaryOperator;
 
 /**
  * Represents an implementation of {@link IAudioPlayer} for the Spigot platform.
@@ -26,10 +22,6 @@ import javax.inject.Singleton;
 @Singleton
 public class AudioPlayerSpigot implements IAudioPlayer
 {
-    /**
-     * Half the range of the audio in blocks in the x, y, and z direction.
-     */
-    private static final int AUDIO_RANGE = 15;
 
     private final IExecutor executor;
 
@@ -40,45 +32,51 @@ public class AudioPlayerSpigot implements IAudioPlayer
     }
 
     @Override
-    public void playSound(ILocation loc, String sound, float volume, float pitch)
+    public void playSound(
+        double x, double y, double z, IWorld world, String sound, float volume, float pitch, double range,
+        @Nullable DoubleUnaryOperator attenuationFunction)
     {
-        playSound(SpigotAdapter.getBukkitLocation(loc), sound, volume, pitch);
+        playSound(
+            new Location(Bukkit.getWorld(world.worldName()), x, y, z),
+            sound, volume, pitch, range, attenuationFunction);
     }
 
-    @Override
-    public void playSound(Vector3Di pos, IWorld world, String sound, float volume, float pitch)
-    {
-        playSound(new Location(Bukkit.getWorld(world.worldName()), pos.x(), pos.y(), pos.z()), sound, volume, pitch);
-    }
-
-    @Override
-    public void playSound(Vector3Dd pos, IWorld world, String sound, float volume, float pitch)
-    {
-        playSound(new Location(Bukkit.getWorld(world.worldName()), pos.x(), pos.y(), pos.z()), sound, volume, pitch);
-    }
-
-    @Override
-    public void playSound(double x, double y, double z, IWorld world, String sound, float volume, float pitch)
-    {
-        playSound(new Location(Bukkit.getWorld(world.worldName()), x, y, z), sound, volume, pitch);
-    }
-
-    /**
-     * Play a sound for all players in a range of {@link #AUDIO_RANGE} blocks around the provided location.
-     */
-    private void playSound(Location loc, String sound, float volume, float pitch)
+    private void playSound(
+        Location loc,
+        String sound,
+        float volume,
+        float pitch,
+        double range,
+        @Nullable DoubleUnaryOperator attenuationFunction)
     {
         final @Nullable World world = loc.getWorld();
         if (world == null)
             return;
 
-        executor.runOnMainThread(() -> playSound(loc, world, sound, volume, pitch, AUDIO_RANGE));
+        executor.runOnMainThread(() -> playSound(loc, world, sound, volume, pitch, range, attenuationFunction));
     }
 
-    private void playSound(Location loc, World world, String sound, float volume, float pitch, int range)
+    private void playSound(
+        Location loc, World world, String sound, float volume, float pitch, double range,
+        @Nullable DoubleUnaryOperator attenuationFunction)
     {
-        for (final Entity ent : world.getNearbyEntities(loc, range, range, range))
+        for (final Entity ent : world.getNearbyEntities(loc, range, range, range, e -> e instanceof Player))
             if (ent instanceof Player player)
-                player.playSound(loc, sound, SoundCategory.BLOCKS, volume, pitch);
+                playSound(player, loc, sound, volume, pitch, attenuationFunction);
+    }
+
+    private void playSound(
+        Player player, Location loc, String sound, float volume, float pitch,
+        @Nullable DoubleUnaryOperator attenuationFunction)
+    {
+        final double distance = player.getLocation().distance(loc);
+        if (distance > AUDIO_RANGE)
+            return;
+
+        float attenuatedVolume = volume;
+        if (attenuationFunction != null)
+            attenuatedVolume *= attenuationFunction.applyAsDouble(distance);
+
+        player.playSound(loc, sound, attenuatedVolume, pitch);
     }
 }
