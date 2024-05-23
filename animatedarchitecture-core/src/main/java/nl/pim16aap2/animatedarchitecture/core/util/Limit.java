e07056ba451f653eed41712866b8f559cb0f5704
Pim van der Loos <pimvanderloos@gmail.com>
2024-05-23 10:19:07 +0200

    Add documentation for Limits

--- animatedarchitecture-core/src/main/java/nl/pim16aap2/animatedarchitecture/core/util/Limit.java
+++ animatedarchitecture-core/src/main/java/nl/pim16aap2/animatedarchitecture/core/util/Limit.java
@@ -6,18 +6,63 @@ import nl.pim16aap2.animatedarchitecture.core.api.IConfig;
 import java.util.OptionalInt;
 import java.util.function.Function;
 
+/**
+ * Represents a limit that can be set for a player.
+ * <p>
+ * Each limit has a user permission and an admin permission. The user permission is used to determine the limit for a
+ * player by checking the highest numerical suffix of the permission node. The admin permission is used to bypass the
+ * limit altogether.
+ */
 public enum Limit
 {
+    /**
+     * The maximum size a structure can have in number of blocks.
+     */
     STRUCTURE_SIZE("doorsize", IConfig::maxStructureSize),
+
+    /**
+     * The maximum number of structures a player can own.
+     */
     STRUCTURE_COUNT("doorcount", IConfig::maxStructureCount),
+
+    /**
+     * The maximum distance a power block can be from a structure.
+     */
     POWERBLOCK_DISTANCE("powerblockdistance", IConfig::maxPowerBlockDistance),
+
+    /**
+     * The maximum distance a structure can move measured in blocks.
+     * <p>
+     * This does not take the number of blocks the structure actually moves into account, only the distance the
+     * structure moves.
+     */
     BLOCKS_TO_MOVE("blockstomove", IConfig::maxBlocksToMove),
     ;
 
+    /**
+     * The permission node for the user to determine the limit.
+     * <p>
+     * The limit for a player is determined by checking the highest numerical suffix of this permission node.
+     *
+     * @return The permission node for the user to determine the limit.
+     */
     @Getter
     private final String userPermission;
+
+    /**
+     * The permission node for an admin to bypass the limit.
+     *
+     * @return The permission node for an admin to bypass the limit.
+     */
     @Getter
     private final String adminPermission;
+
+    /**
+     * The supplier for the global limit.
+     * <p>
+     * This should be a reference to the method in the {@link IConfig} implementation that returns the global limit for
+     * this limit.
+     */
     private final Function<IConfig, OptionalInt> globalLimitSupplier;
 
     Limit(String permissionName, Function<IConfig, OptionalInt> globalLimitSupplier)
@@ -27,6 +72,13 @@ public enum Limit
         this.globalLimitSupplier = globalLimitSupplier;
     }
 
+    /**
+     * Gets the global limit for this limit.
+     *
+     * @param config
+     *     The config to get the global limit from.
+     * @return The global limit for this limit as defined in the config.
+     */
     public OptionalInt getGlobalLimit(IConfig config)
     {
         return globalLimitSupplier.apply(config);
