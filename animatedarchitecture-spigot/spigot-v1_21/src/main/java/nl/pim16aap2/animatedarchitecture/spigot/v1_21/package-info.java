e648b75bcf0e6a27074f5de831bb4eef645b3afc
Pim van der Loos <pimvanderloos@gmail.com>
2024-07-02 17:24:34 +0200

    Add 1.21 module for new materials

---
+++ animatedarchitecture-spigot/spigot-v1_21/src/main/java/nl/pim16aap2/animatedarchitecture/spigot/v1_21/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains all version-specific code for the Spigot v1_21 platform.
+ */
+@NonNullByDefault
+package nl.pim16aap2.animatedarchitecture.spigot.v1_21;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
