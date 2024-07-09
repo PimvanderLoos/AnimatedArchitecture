0c5a9986b721b1a5ed7a582a5a3611be6f561f38
Pim van der Loos <pimvanderloos@gmail.com>
2024-07-09 20:04:18 +0200

    Drop support for MC < 1.20
    - Dropped all support for versions of Minecraft prior to 1.20. It is currently too annoying to get this plugin to run on 1.19 anyway, so this should not have much of an effect. Additionally, bstats shows almost all servers run 1.20 or higher already.

---
+++ animatedarchitecture-spigot/spigot-v1_20/src/main/java/nl/pim16aap2/animatedarchitecture/spigot/v1_20/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains all version-specific code for the Spigot v1_20 platform.
+ */
+@NonNullByDefault
+package nl.pim16aap2.animatedarchitecture.spigot.v1_20;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
