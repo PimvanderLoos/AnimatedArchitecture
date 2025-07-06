8352f5d7b8bd658a68b3bcda5380ddb9a5c4139f
Pim van der Loos <pimvanderloos@gmail.com>
2025-07-06 14:34:51 +0200

    Implement Structures section

---
+++ animatedarchitecture-spigot/spigot-core/src/main/java/nl/pim16aap2/animatedarchitecture/spigot/core/config/StructureSubSectionSpigot.java
@@ -0,0 +1,54 @@
+package nl.pim16aap2.animatedarchitecture.spigot.core.config;
+
+import nl.pim16aap2.animatedarchitecture.core.config.StructureSubSection;
+import nl.pim16aap2.animatedarchitecture.core.config.StructureTypeConfigurationOption;
+import nl.pim16aap2.animatedarchitecture.core.structures.StructureType;
+import org.bukkit.Material;
+
+import java.util.List;
+
+/**
+ * Spigot-specific implementation of {@link StructureSubSection}.
+ * <p>
+ * This class is used to define a subsection for a specific structure type in the configuration.
+ */
+public class StructureSubSectionSpigot extends StructureSubSection
+{
+    public static final String PATH_GUI_MATERIAL = "gui_material";
+
+    public StructureSubSectionSpigot(StructureType structureType)
+    {
+        super(structureType);
+    }
+
+    @Override
+    protected void appendConfigurationOptions(List<StructureTypeConfigurationOption> options)
+    {
+        options.add(getGuiMaterialOption());
+    }
+
+    private StructureTypeConfigurationOption getGuiMaterialOption()
+    {
+        return new StructureTypeConfigurationOption(
+            PATH_GUI_MATERIAL,
+            getDefaultGuiMaterial().name()
+        );
+    }
+
+    private Material getDefaultGuiMaterial()
+    {
+        return switch (getStructureType().getFullKey())
+        {
+            case "animatedarchitecture:bigdoor" -> Material.OAK_DOOR;
+            case "animatedarchitecture:clock" -> Material.CLOCK;
+            case "animatedarchitecture:drawbridge" -> Material.OAK_TRAPDOOR;
+            case "animatedarchitecture:flag" -> Material.WHITE_BANNER;
+            case "animatedarchitecture:garagedoor" -> Material.MINECART;
+            case "animatedarchitecture:portcullis" -> Material.IRON_BARS;
+            case "animatedarchitecture:revolvingdoor" -> Material.MUSIC_DISC_PIGSTEP;
+            case "animatedarchitecture:slidingdoor" -> Material.PISTON;
+            case "animatedarchitecture:windmill" -> Material.SUNFLOWER;
+            default -> Material.BARRIER;
+        };
+    }
+}
