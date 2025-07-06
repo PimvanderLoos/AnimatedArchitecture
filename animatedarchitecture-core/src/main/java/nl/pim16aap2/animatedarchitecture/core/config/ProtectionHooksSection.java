ffe22c877ed9c4fa54aa6550491e3356e698c507
Pim van der Loos <pimvanderloos@gmail.com>
2025-07-06 15:14:33 +0200

    Implement protection hooks section

--- animatedarchitecture-core/src/main/java/nl/pim16aap2/animatedarchitecture/core/config/ProtectionHooksSection.java
+++ animatedarchitecture-core/src/main/java/nl/pim16aap2/animatedarchitecture/core/config/ProtectionHooksSection.java
@@ -9,7 +9,7 @@ import org.spongepowered.configurate.serialize.SerializationException;
  * This section is used to configure hooks for various protection plugins, allowing the plugin to interact with them for
  * structure protection and other related features.
  */
-public class ProtectionHooksSection implements IConfigSection
+public abstract class ProtectionHooksSection implements IConfigSection
 {
     public static final String SECTION_TITLE = "protection_hooks";
 
@@ -17,7 +17,39 @@ public class ProtectionHooksSection implements IConfigSection
     public CommentedConfigurationNode buildInitialLimitsNode()
         throws SerializationException
     {
-        throw new UnsupportedOperationException("Protection hooks section is not implemented yet.");
+        return CommentedConfigurationNode.root()
+            .comment("""
+                Enable or disable compatibility hooks for certain plugins.
+                
+                If the plugins aren't installed, these options do nothing.
+                
+                When enabled, structures cannot be toggled or created in areas not owned by the owner of that structure.
+                """)
+            .set("[]");
+    }
+
+    public abstract void populateProtectionHooks(CommentedConfigurationNode root)
+        throws SerializationException;
+
+    /**
+     * Gets the subsection for the protection hooks section in the configuration.
+     * <p>
+     * If the subsection does not exist, it will be created.
+     *
+     * @param root
+     *     The root configuration node to get the protection hooks subsection from.
+     * @return The protection hooks subsection node.
+     *
+     * @throws SerializationException
+     *     If there is an error during serialization.
+     */
+    protected CommentedConfigurationNode getProtectionHooksSubSection(CommentedConfigurationNode root)
+        throws SerializationException
+    {
+        final CommentedConfigurationNode node = root.node(getSectionTitle());
+        if (node.virtual())
+            return buildInitialLimitsNode();
+        return node;
     }
 
     @Override
