199f4c2dc8786f5900349bb47a1bea93c60eee74
Pim van der Loos <pimvanderloos@gmail.com>
2025-07-26 16:23:37 +0200

    Allow inserting root path as parameter for filesystemtest

--- utilities/test-util/src/main/java/nl/pim16aap2/testing/extensions/FilesystemExtension.java
+++ utilities/test-util/src/main/java/nl/pim16aap2/testing/extensions/FilesystemExtension.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
 import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider;
 
 import java.nio.file.FileSystem;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -24,25 +25,37 @@ import java.util.stream.Stream;
  * Jimfs in-memory filesystem. Each test method annotated with {@link FileSystemTest} will be executed once for each
  * platform configuration.
  * <p>
- * To use this extension, annotated your test method with {@link FileSystemTest}. The extension will inject a
- * {@link FileSystem} parameter into your test methods, providing access to the configured filesystem for that test
- * invocation.
+ * To use this extension, annotated your test method with {@link FileSystemTest}. The extension can inject either a
+ * {@link FileSystem} or {@link Path} parameter into your test methods:
+ * <ul>
+ *   <li>{@link FileSystem} - provides access to the configured filesystem for that test invocation</li>
+ *   <li>{@link Path} - provides a root path within the configured filesystem</li>
+ * </ul>
  * <p>
  * Example usage:
  * <pre> {@code
  * class MyFileSystemTest {
  *     @FileSystemTest
- *     void testCreateDirectory(FileSystem fs) {
+ *     void testWithFileSystem(FileSystem fs) {
  *         Path testDir = fs.getPath("test");
  *         Files.createDirectory(testDir);
  *         assertTrue(Files.exists(testDir));
+ *     }
+ *
+ *     @FileSystemTest
+ *     void testWithPath(Path rootPath) {
+ *         ...
+ *     }
+ *
+ *     @FileSystemTest
+ *     void testWithBoth(FileSystem fs, Path rootPath) {
+ *         ...
  *     }
  * }
  * } </pre>
  * <p>
  * The filesystem is automatically created before each test invocation and closed after each test to ensure test
- * isolation. Each platform configuration provides different path separators, case sensitivity rules, and other
- * filesystem behaviors specific to that operating system.
+ * isolation.
  */
 public class FilesystemExtension implements TestTemplateInvocationContextProvider, ParameterResolver
 {
@@ -76,16 +89,25 @@ public class FilesystemExtension implements TestTemplateInvocationContextProvide
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
         throws ParameterResolutionException
     {
-        return parameterContext.getParameter().getType().equals(FileSystem.class);
+        final Class<?> paramType = parameterContext.getParameter().getType();
+        return paramType.equals(FileSystem.class) || paramType.equals(Path.class);
     }
 
     @Override
     public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
         throws ParameterResolutionException
     {
-        return extensionContext
+        final FileSystem fs = extensionContext
             .getStore(EXTENSION_NAMESPACE)
             .get(FILE_SYSTEM_KEY, FileSystem.class);
+
+        return switch (parameterContext.getParameter().getType().getSimpleName())
+        {
+            case "FileSystem" -> fs;
+            case "Path" -> fs.getRootDirectories().iterator().next();
+            default -> throw new ParameterResolutionException(
+                "Unsupported parameter type: " + parameterContext.getParameter().getType());
+        };
     }
 
     private static class JimfsInvocationContext implements TestTemplateInvocationContext
